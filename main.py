28c63e1cdb2db745276edee7fa4a4006f892c689
Máximo Sojo <maxsojo13@gmail.com>
2021-05-17 16:51:16 -0400

    refactor(Command): Se agrega help y comandos existentes.

--- main.py
+++ main.py
@@ -28,3 +28,55 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
+
+def start(update, context):
+    user = update.message.from_user
+    bot_msg = "Saludos {}, bienvenido.\n\n" \
+              "¿Que soy?.\n" \
+              "Soy el Bot Wallet de Hive, conmigo puede tener en su Telegram un resumen a tiempo real de su wallet en hive. Naci a modo de prueba y mi nacimiendo esta en un post publico en Hive\n\n" \
+              "Use el comando /help para conocer las opciones disponibles.\n\n" \
+              "La publicación donde naci https://maximosojo.github.io/myhivewallet-bot\n" \
+              "Mi código fuente esta en https://maximosojo.github.io/myhivewallet-bot\n" \
+              "Perfil de mi creador https://peakd.com/@maximosojo\n\n" \
+              "Nota: El bot no registra ningun tipo de información, esto puede ser verificado en su código fuente.\n\n" \
+              "Disfrute del bot. Gracias por estar aquí! \n\n".format(user.first_name)
+    update.message.reply_text(bot_msg)
+
+def help(update, context):
+    """Enviar un mensaje cuando se emite el comando /help"""
+    bot_msg = "Comandos disponibles:\n" \
+              "/start Inicio del bot\n" \
+              "/sumarry Buscar el resumen propio (Busca el resumen segun su nombre de usuario de Telegram)\n" \
+              "/search Buscar el resumen de un usuario (Solicita el nombre del usuario a consultar)\n" \
+              "/exit Finalizar la conversación con el bot\n"
+    update.message.reply_text(bot_msg)
+
+def error(update, context):
+    """Log de errors."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+def main():
+    """Configuración inicial de bot."""
+    # Registro de token previamente registrado.
+    updater = Updater(Config.getBotToken(), use_context=True)
+
+    # Obtiene el event dispatcher para registrar las acciones del bot
+    dp = updater.dispatcher
+
+    # Inicia el bot
+    dp.add_handler(CommandHandler("start", start))
+    
+    # Ayuda del bot
+    dp.add_handler(CommandHandler("help", help))
+
+    # Retorna errores
+    dp.add_error_handler(error)
+
+    # Inicia el bot
+    updater.start_polling()
+
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
